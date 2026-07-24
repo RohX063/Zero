a33548bcd88a8308ece8725d2faf1f738f9dcd5d
Rohan <rohan27980@gmail.com>
2026-07-25 03:00:22 +0530

    Enhance UCI loop with move promotion support
    Refactor UCI loop to include move promotion handling and improve command processing.

--- src/uci.cpp
+++ src/uci.cpp
@@ -1,16 +1,160 @@
 #include "uci.h"
 
 #include "board.h"
-#include "search.h"
 #include "movegen.h"
-#include "tt.h"
-#include "history.h"
-#include "killer.h"
+#include "search.h"
 
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
+
+namespace
+{
+std::string toUciMove(const Move& move)
+{
+    std::string uciMove;
+    uciMove += char('a' + move.fromCol);
+    uciMove += char('8' - move.fromRow);
+    uciMove += char('a' + move.toCol);
+    uciMove += char('8' - move.toRow);
+
+    if(move.isPromotion)
+    {
+        switch(move.promotionPiece)
+        {
+            case WHITE_QUEEN:
+            case BLACK_QUEEN:
+                uciMove += 'q';
+                break;
+            case WHITE_ROOK:
+            case BLACK_ROOK:
+                uciMove += 'r';
+                break;
+            case WHITE_BISHOP:
+            case BLACK_BISHOP:
+                uciMove += 'b';
+                break;
+            case WHITE_KNIGHT:
+            case BLACK_KNIGHT:
+                uciMove += 'n';
+                break;
+            default:
+                break;
+        }
+    }
+
+    return uciMove;
+}
+
+bool applyMove(Board& board, const std::string& moveText)
+{
+
+    const std::vector<Move> legalMoves =
+        generateLegalMoves(board, board.isWhiteToMove());
+
+    for (const Move& legalMove : legalMoves)
+    {
+        if (toUciMove(legalMove) == moveText)
+        {
+
+            Move move = legalMove;
+            board.makeMove(move);
+
+            return true;
+        }
+    }
+    return false;
+}
+
+void setPosition(Board& board, const std::string& command)
+{
+
+    const std::string prefix = "position ";
+    if(command.rfind(prefix, 0) != 0)
+        return;
+
+    std::string body = command.substr(prefix.size());
+
+    if(body.rfind("startpos", 0) == 0)
+    {
+        board = Board();
+        body = body.substr(8);
+    }
+    else if(body.rfind("fen ", 0) == 0)
+    {
+        std::string fenAndMoves = body.substr(4);
+        const size_t movesPos = fenAndMoves.find(" moves ");
+
+        std::string fen = fenAndMoves;
+        std::string movesText;
+
+        if(movesPos != std::string::npos)
+        {
+            fen = fenAndMoves.substr(0, movesPos);
+            movesText = fenAndMoves.substr(movesPos + 7);
+        }
+
+        if(!board.loadFEN(fen))
+        {
+            board = Board();
+            return;
+        }
+
+        if(!movesText.empty())
+        {
+            std::stringstream ss(movesText);
+            std::string moveText;
+            while(ss >> moveText)
+            {
+                if(!applyMove(board, moveText))
+                    break;
+            }
+        }
+
+
+        return;
+    }
+    else
+    {
+        return;
+    }
+
+    if(!body.empty() && body[0] == ' ')
+        body.erase(0, 1);
+
+    const size_t movesPos = body.find("moves ");
+    if(movesPos != std::string::npos)
+    {
+        std::stringstream ss(body.substr(movesPos + 6));
+        std::string moveText;
+        while(ss >> moveText)
+        {
+            if(!applyMove(board, moveText))
+                break;
+        }
+    }
+}
+
+int parseGoDepth(const std::string& command)
+{
+    std::stringstream ss(command);
+    std::string token;
+    int depth = 0;
+
+    while(ss >> token)
+    {
+        if(token == "depth")
+        {
+            ss >> token;
+            depth = std::stoi(token);
+            break;
+        }
+    }
+
+    return depth > 0 ? depth : 5;
+}
+} // namespace
 
 void uciLoop()
 {
@@ -19,167 +163,50 @@ void uciLoop()
 
     while(std::getline(std::cin, command))
     {
-        std::cerr << "RECV: [" << command << "]" << std::endl;
+        if(command.empty())
+            continue;
 
         if(command == "uci")
         {
             std::cout << "id name Zero" << std::endl;
             std::cout << "id author Rohan Singh" << std::endl;
+            std::cout << "option name Hash type spin default 64 min 1 max 1024" << std::endl;
             std::cout << "uciok" << std::endl;
         }
         else if(command == "isready")
         {
-            std::cout << "readyok\n";
+            std::cout << "readyok" << std::endl;
         }
         else if(command == "quit")
         {
             break;
         }
+        else if(command == "ucinewgame")
+        {
+            board = Board();
+        }
+        else if(command.rfind("position ", 0) == 0)
+        {
+            setPosition(board, command);
+        }
         else if(command.rfind("go", 0) == 0)
         {
-            std::cerr << "GO RECEIVED\n";
-
-            std::cerr << "Side to move = "
-                      << (board.isWhiteToMove() ? "White" : "Black")
-                      << '\n';
-
-            std::cerr << "Generating moves...\n";
-
-            auto legalMoves = generateLegalMoves(board, board.isWhiteToMove());
-
-            std::cerr << "Legal move count = "
-                      << legalMoves.size()
-                      << '\n';
-
-            Move bestMove = iterativeDeepening(board, 5);
-
-            std::cerr << "Search finished\n";
-
-            std::cout << "bestmove "
-                      << char('a' + bestMove.fromCol)
-                      << (8 - bestMove.fromRow)
-                      << char('a' + bestMove.toCol)
-                      << (8 - bestMove.toRow)
-                      << std::endl;
+            board.printBoard();
+            std::cout
+            << "GO COMMAND: side = "
+            << (board.isWhiteToMove() ? "WHITE" : "BLACK")
+            << std::endl;
+            const int depth = parseGoDepth(command);
+            const Move bestMove = iterativeDeepening(board, depth);
+            std::cout << "bestmove " << toUciMove(bestMove) << std::endl;
         }
-        else if(command.rfind("position startpos", 0) == 0)
+        else if(command.rfind("setoption ", 0) == 0)
         {
-            if(!board.loadFEN(
-                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w"
-            ))
-            {
-                std::cerr << "Failed to load start position\n";
-            }
-
-            const size_t movesPos = command.find("moves");
-
-            if(movesPos != std::string::npos)
-            {
-                std::string movesString = command.substr(movesPos + 6);
-                std::stringstream ss(movesString);
-                std::string moveString;
-
-                while(ss >> moveString)
-                {
-                    std::cerr << "Move = " << moveString << '\n';
-
-                    std::vector<Move> legalMoves =
-                        generateLegalMoves(board, board.isWhiteToMove());
-
-                    bool found = false;
-
-                    for(Move &move : legalMoves)
-                    {
-                        std::string uciMove;
-
-                        uciMove += char('a' + move.fromCol);
-                        uciMove += char('8' - move.fromRow);
-                        uciMove += char('a' + move.toCol);
-                        uciMove += char('8' - move.toRow);
-
-                        // Promotion
-                        if(move.isPromotion)
-                        {
-                            switch(move.promotionPiece)
-                            {
-                                case WHITE_QUEEN:
-                                case BLACK_QUEEN:
-                                    uciMove += 'q';
-                                    break;
-
-                                case WHITE_ROOK:
-                                case BLACK_ROOK:
-                                    uciMove += 'r';
-                                    break;
-
-                                case WHITE_BISHOP:
-                                case BLACK_BISHOP:
-                                    uciMove += 'b';
-                                    break;
-
-                                case WHITE_KNIGHT:
-                                case BLACK_KNIGHT:
-                                    uciMove += 'n';
-                                    break;
-                            }
-                        }
-
-                        if(uciMove == moveString)
-                        {
-                            board.makeMove(move);
-                            found = true;
-
-                            std::cerr
-                                << "Applied: "
-                                << uciMove
-                                << " | Side to move = "
-                                << (board.isWhiteToMove() ? "White" : "Black")
-                                << '\n';
-
-                            break;
-                        }
-                    }
-
-                    if(!found)
-                    {
-                        std::cerr << "Invalid move: " << moveString << '\n';
-
-                        std::cerr << "Legal moves are:\n";
-
-                        for(const Move &m : legalMoves)
-                        {
-                            std::string u;
-
-                            u += char('a' + m.fromCol);
-                            u += char('8' - m.fromRow);
-                            u += char('a' + m.toCol);
-                            u += char('8' - m.toRow);
-
-                            if(m.isPromotion)
-                            {
-                                switch(m.promotionPiece)
-                                {
-                                    case WHITE_QUEEN:
-                                    case BLACK_QUEEN:   u += 'q'; break;
-
-                                    case WHITE_ROOK:
-                                    case BLACK_ROOK:    u += 'r'; break;
-
-                                    case WHITE_BISHOP:
-                                    case BLACK_BISHOP:  u += 'b'; break;
-
-                                    case WHITE_KNIGHT:
-                                    case BLACK_KNIGHT:  u += 'n'; break;
-                                }
-                            }
-
-                            std::cerr << u << '\n';
-                        }
-
-                        break;
-                    }
-                }
-            }
+            // Ignore for now; cutechess only needs basic UCI compliance.
+        }
+        else if(command.rfind("stop", 0) == 0)
+        {
+            // Search is single-shot in this implementation.
         }
     }
 }
